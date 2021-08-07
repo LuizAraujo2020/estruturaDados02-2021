af99565f6c661a8b228dd21ac10125c80f8dfa34
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-07 12:04:59 -0300

    :gift: Main.c

---
+++ estruturaDados_01/estruturaDados_01/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  estruturaDados_01
+//
+//  Created by Luiz Carlos da Silva Araujo on 07/08/21.
+//
+
+#include <stdio.h>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    
+    
+    return 0;
+}
